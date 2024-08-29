dd77292da30e8e83e912d596d7fee65ec58779ed
Rahul-0410 <rgs786999@gmail.com>
2024-08-29 22:16:01 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/08/newsroom-robert-noyce-bldg-2.jpg.rendition.intel_.web_.1920.1080.jpg?resize=900,506" alt="img"/>
+        </div>
+        <div className="texts">
+        <h2>IBM Cloud to offer Intel’s Gaudi 3 AI chips next year</h2>
+        <p className="info">
+          <a className="author">Rahul </a>
+          <time> 2024-08-29 22:06</time>
+        </p>
+        <p className="summary">Intel has found its first cloud customer for its Gaudi 3 AI accelerator chip: IBM Cloud.
+IBM and Intel on Thursday said IBM Cloud will begin offering Gaudi 3 to customers early next year. 
+The chip’s accelerators will be available for both hybrid and on-premise environments, and IBM says it plans to enable support for Gaudi
+ 3 within its Watsonx AI and data platform.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/08/newsroom-robert-noyce-bldg-2.jpg.rendition.intel_.web_.1920.1080.jpg?resize=900,506" alt="img"/>
+        </div>
+        <div className="texts">
+        <h2>IBM Cloud to offer Intel’s Gaudi 3 AI chips next year</h2>
+        <p className="info">
+          <a className="author">Rahul </a>
+          <time> 2024-08-29 22:06</time>
+        </p>
+        <p className="summary">Intel has found its first cloud customer for its Gaudi 3 AI accelerator chip: IBM Cloud.
+IBM and Intel on Thursday said IBM Cloud will begin offering Gaudi 3 to customers early next year. 
+The chip’s accelerators will be available for both hybrid and on-premise environments, and IBM says it plans to enable support for Gaudi
+ 3 within its Watsonx AI and data platform.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/08/newsroom-robert-noyce-bldg-2.jpg.rendition.intel_.web_.1920.1080.jpg?resize=900,506" alt="img"/>
+        </div>
+        <div className="texts">
+        <h2>IBM Cloud to offer Intel’s Gaudi 3 AI chips next year</h2>
+        <p className="info">
+          <a className="author">Rahul </a>
+          <time> 2024-08-29 22:06</time>
+        </p>
+        <p className="summary">Intel has found its first cloud customer for its Gaudi 3 AI accelerator chip: IBM Cloud.
+IBM and Intel on Thursday said IBM Cloud will begin offering Gaudi 3 to customers early next year. 
+The chip’s accelerators will be available for both hybrid and on-premise environments, and IBM says it plans to enable support for Gaudi
+ 3 within its Watsonx AI and data platform.</p>
+        </div>
+      </div>
+      
+      
+      
+
+    </main>
   );
 }
 
