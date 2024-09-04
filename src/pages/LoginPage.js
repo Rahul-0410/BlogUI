e1a39fb422cbabc3a0bce8e9789e7e7b8a164349
Rahul-0410 <rgs786999@gmail.com>
2024-09-04 22:09:54 +0530

    login and signup add

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { Form } from 'react-router-dom'
-
+import {Navigate} from 'react-router-dom';
 function LoginPage() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [redirect, setRedirect] = React.useState(false);
+
+ async function login(e){
+    e.preventDefault();
+ const response=  await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        username,
+        password
+      }),
+      credentials:'include',
+    })
+    if(response.ok){
+        // alert('login success');
+        setRedirect(true);
+    } else {
+        alert('login failed');
+    }
+
+    setPassword('');
+    setUsername('');
+  }
+
+  if(redirect){
+    return <Navigate to='/'/>
+  }
   return (
-    <form className='login'>
+    <form className='login' onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="username" value={username} 
+        onChange={(e)=>setUsername(e.target.value)}/>
+
+        <input type="password" placeholder="password"
+        value={password} 
+        onChange={(e)=>setPassword(e.target.value)}/>
         <button type="submit">Login</button>
     </form>
   )
