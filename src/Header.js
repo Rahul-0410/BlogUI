fe8f6bea0252cd849318c29040c4a1fdb564d27b
Rahul-0410 <rgs786999@gmail.com>
2024-09-05 18:56:12 +0530

    done

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,47 @@
+import { set } from 'mongoose';
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+  const [username, setUsername] = React.useState(null)
+  React.useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include'
+    }).then(res => {
+      res.json().then(userinfo=> {
+          setUsername(userinfo.username);
+      })
+    })
+      
+  },[]);
+
+    function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST'
+    })
+    setUsername(null);
+    }
+
   return (
     <header>
         <Link to="/" className="logo">MyBlog</Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {username && 
+          <>
+          <Link to="/create">Create new Post</Link>
+          <a onClick={logout}>Logout</a>
+          </>
+          
+          }
+          
+          {!username && 
+          <>
+           <Link to="/login">Login</Link>
+           <Link to="/register">Register</Link>
+          </>
+          }
+         
         </nav>
       </header>
   )
