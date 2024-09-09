26734e9b2cf6490fb1646405a735a39ffa7d7e28
Rahul-0410 <rgs786999@gmail.com>
2024-09-09 11:50:29 +0530

    done

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,66 +1,48 @@
-import React from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'
+import 'react-quill/dist/quill.snow.css';
+import {useState} from "react";
+import {Navigate} from "react-router-dom";
+import Editor from "../Editor";
 
-const modules = {
-    toolbar: [
-        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-        [{size: []}],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, 
-         {'indent': '-1'}, {'indent': '+1'}],
-        ['link', 'image', 'video'],
-        ['clean']
-      ],
-}
-
-const formats= [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 'video'
-]
-
-function CreatePost() {
-    const [title, setTitle] = React.useState('');
-    const [summary, setSummary] = React.useState('');
-    const [content, setContent] = React.useState('');
-   const [files, setFiles] = React.useState('');
-
-   async function createNewPost(e){
-        const data = new FormData();
-        data.set('title', title);
-        data.set('summary', summary);
-        data.set('content', content);
-        data.set('file', files[0]);
-            e.preventDefault();
-            // console.log(file);
-            
-        const response= await fetch('http://localhost:4000/post',{
-            method: 'POST',
-            body: data,
-        });
-          console.log( await response.json());
-           
-            // .then(()=> {
-            //     setTitle('');
-            //     setSummary('');
-            //     setContent('');
-    
+export default function CreatePost() {
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  async function createNewPost(ev) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
     }
+  }
 
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
   return (
     <form onSubmit={createNewPost}>
-        <input type="title" placeholder={'Title'}
-        value={title} onChange={(e)=>setTitle(e.target.value)}/>
-        <input type="summary" placeholder={'Summary'}
-        value={summary} onChange={(e)=> setSummary(e.target.value)}/>
-        <input type="file"  onChange={(e)=>setFiles(e.target.files)}/>
-        <ReactQuill value={content} modules={modules} formats={formats}
-        onChange={(newValue)=>setContent(newValue)}/>
-        <button style={{marginTop: '6px'}}>Create post</button>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{marginTop:'5px'}}>Create post</button>
     </form>
-  )
+  );
 }
-
-export default CreatePost
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Post from '../Post'
 
 function IndexPage() {
+  useEffect(()=>{
+    fetch('http://localhost:4000/post').then(response =>{
+      response.json().then((posts=>{
+        console.log(posts);
+        
+      }));
+    })
+  },[])
   return (
     <>
     <Post/>
