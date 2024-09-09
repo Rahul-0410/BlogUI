a8f5709abb7bf1843e65174a50207f837c87a789
Rahul-0410 <rgs786999@gmail.com>
2024-09-09 19:41:49 +0530

    done

--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,20 @@
 import React from 'react'
+import {format} from 'date-fns'
 
-function Post() {
+function Post({title,summary,cover,content, createdAt, author}) {
   return (
     <div className="post">
     <div className="image">
-    <img src="https://techcrunch.com/wp-content/uploads/2023/08/newsroom-robert-noyce-bldg-2.jpg.rendition.intel_.web_.1920.1080.jpg?resize=900,506" alt="img"/>
+    <img src={'http://localhost:4000/'+cover} alt="img"/>
     </div>
     <div className="texts">
-    <h2>IBM Cloud to offer Intel’s Gaudi 3 AI chips next year</h2>
+    <h2>{title}</h2>
     <p className="info">
-      <a className="author">Rahul </a>
-      <time> 2024-08-29 22:06</time>
+      <a className="author">{author.username}</a>
+      {/* <time> 2024-08-29 22:06</time> */}
+      <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
     </p>
-    <p className="summary">Intel has found its first cloud customer for its Gaudi 3 AI accelerator chip: IBM Cloud.
-IBM and Intel on Thursday said IBM Cloud will begin offering Gaudi 3 to customers early next year. 
-</p>
+    <p className="summary">{summary}</p>
     </div>
   </div> 
   )
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,19 +2,21 @@ import React, { useEffect } from 'react'
 import Post from '../Post'
 
 function IndexPage() {
+  const [posts, setPosts] = React.useState([]);
   useEffect(()=>{
     fetch('http://localhost:4000/post').then(response =>{
       response.json().then((posts=>{
-        console.log(posts);
+        setPosts(posts);
+        // console.log(posts);
         
       }));
     })
   },[])
   return (
     <>
-    <Post/>
-    <Post/>
-    <Post/>
+    {posts.length>0 && posts.map((post,index)=>(
+      <Post {...post} key={index}/>
+    ))}
     </>
   )
 }
